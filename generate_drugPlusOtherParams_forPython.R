8b9e7d5dcd0eed0b1aaafbcd2d57420f135aebd5
chris sainsbury <chris@sainsbury.im>
2017-10-23 22:10:09 +0100

    .

--- generate_drugPlusOtherParams_forPython.R
+++ generate_drugPlusOtherParams_forPython.R
@@ -26,16 +26,59 @@ hba1cData <- addIdentifier(hba1cData, 'hba1c_')
 sbpData <- addIdentifier(sbpData, 'sbp_')
 bmiData <- addIdentifier(bmiData, 'bmi_')
 
+# generate merged dataset
 twoParamMerge <- merge(hba1cData, sbpData, by.x = 'interpolatedTS_mortality.LinkId', by.y = 'interpolatedTS_mortality.LinkId')
 threeParamMerge <- merge(twoParamMerge, bmiData, by.x = 'interpolatedTS_mortality.LinkId', by.y = 'interpolatedTS_mortality.LinkId')
-
-x = merge(drugCombinationData_numerical, hba1cData, )
-
-
-# generate merged dataset
-
+fourParamMerge= merge(drugCombinationData_numerical, threeParamMerge, by.x = 'drugWordFrame_forAnalysis.LinkId', by.y = 'interpolatedTS_mortality.LinkId')
 
 # generate outcome data for 1y delta hba1c
+testPeriodYears <- 1
+runinPeriodDurationYears <- 5
+NumberOfTimeBins <- 30
+
+numberOfBinsMakingUpTestPeriod <- (NumberOfTimeBins / runinPeriodDurationYears) * testPeriodYears
+
+paramDifference <- function(inputFrame, numberOfBins) {
+  
+   lastBin = (length(sequence) - 1)
+   firstBin = lastBin - numberOfBins
+   
+   difference <- inputFrame[, lastBin] - inputFrame[, firstBin]
+   
+   differenceFrame <- data.frame(inputFrame$interpolatedTS_mortality.LinkId, difference)
+   
+   return(differenceFrame)
+}
+
+# these are outcomes for the full hba1c set - need to merge back with drug dataset
+hba1cDataOutcome <- paramDifference(hba1cData, numberOfBinsMakingUpTestPeriod)
+sbpDataOutcome <- paramDifference(sbpData, numberOfBinsMakingUpTestPeriod)
+bmiDataOutcome <- paramDifference(bmiData, numberOfBinsMakingUpTestPeriod)
+
+# export the files for python import:
+# needs to be improved. done manually for now
+drug_export <- fourParamMerge[, 2:31]
+hba1c_export <- fourParamMerge[, 32:61]
+sbp_export <- fourParamMerge[, 67:96]
+bmi_export <- fourParamMerge[, 102:131]
+
+## zero the last n bins for final export:
+zeroFinalBins <- function(inputFrame, numberOfBinsToZero) {
+  startZero <- (ncol(inputFrame) - numberOfBinsToZero) + 1
+  inputFrame[, startZero : ncol(inputFrame)] = 0
+  
+  return(inputFrame)
+}
+
+hba1c_export <- zeroFinalBins(hba1c_export, numberOfBinsMakingUpTestPeriod)
+sbp_export <- zeroFinalBins(sbp_export, numberOfBinsMakingUpTestPeriod)
+bmi_export <- zeroFinalBins(bmi_export, numberOfBinsMakingUpTestPeriod)
+
+
+
+
+
+
 
 # manage last year of numerical paramters
 # ? lvcf, ?na, ?zero value
