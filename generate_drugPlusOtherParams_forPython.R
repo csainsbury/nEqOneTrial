e461dc13bdbf5c97fb16dbb8a239713135295330
chris sainsbury <chris@sainsbury.im>
2017-10-23 20:01:40 +0100

    .

--- generate_drugPlusOtherParams_forPython.R
+++ generate_drugPlusOtherParams_forPython.R
@@ -8,7 +8,27 @@ hba1cData <- read.csv("~/R/_workingDirectory/nEqOneTrial/sourceData/hba1c_data_5
 sbpData <- read.csv("~/R/_workingDirectory/nEqOneTrial/sourceData/sbp_data_5y_2008-13_T2.csv", header = T)
 bmiData <- read.csv("~/R/_workingDirectory/nEqOneTrial/sourceData/bmi_data_5y_2008-13_T2.csv", header = T)
 
+# add identifiers to data frames:
+addIdentifier <- function(inputFrame, nameVariable) {
+  
+  colnamesInputFrame <- colnames(inputFrame)
+    
+    for (j in seq(1, (length(sequence) - 1), 1)) {
+      colnamesInputFrame[j] <- paste(nameVariable, 'V', j, sep = '')
+    }
+    
+  colnames(inputFrame) <- colnamesInputFrame
+  
+  return(inputFrame)
+  
+}
+hba1cData <- addIdentifier(hba1cData, 'hba1c_')
+sbpData <- addIdentifier(sbpData, 'sbp_')
+bmiData <- addIdentifier(bmiData, 'bmi_')
+
+
 # generate merged dataset
+
 
 # generate outcome data for 1y delta hba1c
 
